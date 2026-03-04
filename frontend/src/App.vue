e761044e2fe97f9b810c6dfc24c7726c6c1c1230
allen <sdfsds5271463@gmail.com>
2026-03-04 23:13:56 +0800

    test vue change git trigger

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,6 +16,7 @@ import HelloWorld from './components/HelloWorld.vue';
     const response = await fetch("/api/stockDB?start=2026-02-01&end=2026-12-31");
     const data = await response.text();
     console.log(data);
+    console.log(7788);
   },
 })
 export default class App extends Vue {}
